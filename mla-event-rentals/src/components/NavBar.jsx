6b93ab3f15b63cfb44803e0ddb32145000cfa705
Abraham Mirambeaux <mirambeaux7@gmail.com>
2022-07-07 10:50:02 -0400

    updates

--- mla-event-rentals/src/components/NavBar.jsx
+++ mla-event-rentals/src/components/NavBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-// import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
 //import styled from "styled-components"
 
 // const Container = styled.div `
@@ -12,19 +15,29 @@ const NavBar = () => {
     return (
         <div className='Container'>
             <div className='Wrapper'>
-
                 <div className='Left'>
-                   <div> EN </div>
-                   <div>  input 
-                    icon
-                    {/* <SearchIcon/> */}
-                     </div>
+
+             
+                    <div className='input'> 
+                        <SearchIcon />
+                        <div className='inputBorder'> </div>
+                    </div> 
+
+
                 </div>
-                <div  className='Center'>
-                    Center
+                <div className='Center'>
+                  <div className='Logo'> MLA Rentals </div>
                 </div>
-                <div  className='Right'>
-                    Right
+                <div className='Right'>
+                    <div className="Badage"> </div>
+            
+
+                <Badge badgeContent={4} color="primary">
+  <ShoppingCartIcon color="action" />
+</Badge>
+                <div> Register </div>
+                
+                <div> Sign In </div>
                 </div>
 
             </div>
