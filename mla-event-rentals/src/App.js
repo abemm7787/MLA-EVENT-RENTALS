06bd1ad0da7c0047331d0cebb7d34fd7bce3df39
Abraham Mirambeaux <mirambeaux7@gmail.com>
2022-07-13 20:30:53 -0400

    updates

--- mla-event-rentals/src/App.js
+++ mla-event-rentals/src/App.js
@@ -3,11 +3,13 @@ import Home from "./components/Home"
 import React from 'react'
 import ProductList from "./components/Products"
 import Register from "./components/Register"
+import Cart from "./components/Cart"
 function App() {
 
   // return <Home/>
   // return <ProductList/>
-  return <Register/> 
+  // return <Register/> 
+  return <Cart/>
 
 }
 
